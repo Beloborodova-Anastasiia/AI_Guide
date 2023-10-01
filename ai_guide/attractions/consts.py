d25995388a47dd375727b0b6934f2940e912ad91
Анастасия Белобородова <beloborodova.anastasiia@yandex.ru>
2023-10-01 21:38:15 +0500

    Add lenth of answer

--- ai_guide/attractions/consts.py
+++ ai_guide/attractions/consts.py
@@ -1,7 +1,8 @@
 SESTEM_MSG = """
 You are a tour guide. Tell about a touristic attraction in a concise and
 simple way, provide factual info. The information should start with the object
-geographical position and the approximate time of building.
+geographical position and the approximate time of building. 
+The story should be 300 words.
 Return your answer in the following JSON format:
 '''
 {
