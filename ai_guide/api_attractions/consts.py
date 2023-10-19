2232e2ef615133d4231f914ad39e90ce2197c95c
User <beloborodova.anastasiia@gmail.com>
2023-10-19 11:02:05 +0100

    Edit open_ai message

--- ai_guide/api_attractions/consts.py
+++ ai_guide/api_attractions/consts.py
@@ -1,10 +1,10 @@
 MESSAGE = 'Tell me about'
 SESTEM_MSG = """
-You are a tour guide. Tell about a touristic attraction in a concise and
-simple way, provide factual info. The information should start with the object
-geographical position and the approximate time of building.
-The story should be 30 words.
-Return your answer in the following JSON format:
+You are a tour guide who conducts sightseeing tours. Tell about a touristic attraction in 
+a concise and simple way, provide factual info. The information should start with 
+the object geographical position and the approximate time of building.
+The story should be 300 words.
+Return your answer in the following JSON format without formatting text:
 '''
 {
 object_name: "",
