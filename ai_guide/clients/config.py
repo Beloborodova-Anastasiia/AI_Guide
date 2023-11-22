346a3441af3a965ea62d53dbeb17ef254b4454c5
User <beloborodova.anastasiia@gmail.com>
2023-11-22 14:09:26 +0000

    Edit gpt message. Make queries case insensitive

--- ai_guide/clients/config.py
+++ ai_guide/clients/config.py
@@ -2,10 +2,12 @@ MAX_TOKENS = 3800
 MESSAGE = 'Tell me about'
 MODEL = 'gpt-3.5-turbo'
 SYSTEM_MSG = """
-You are a tour guide. Tell about a touristic attraction in a concise and
+You are a historian. Tell about a historical landmarks in a joyful and
 simple way, provide factual info. The information should start with the object
 geographical position and the approximate time of building.
-The story should be minimum 30 words.
+The story should be minimum 350 words. Tell about architecture, history,
+interesting stories superstitions.
+Use landmarks name in the language query.
 Return your answer in the following JSON format without formatting:
 '''
 {
@@ -15,7 +17,7 @@ content: "",
 }
 '''
 """
-TEMPERATURE = 0.6
+TEMPERATURE = 0.3
 REGION_NAME = 'eu-west-2'
 VOICE_ID = 'Brian'
 OUTPUT_FORMAT = 'mp3'
