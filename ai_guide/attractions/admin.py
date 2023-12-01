568bf2c9cb0e3f932cf66779a0c7ada89415ca35
Beloborodova-Anastasiia <98510450+Beloborodova-Anastasiia@users.noreply.github.com>
2023-12-01 14:57:48 +0500

    add admin page (#38)
    Co-authored-by: User <beloborodova.anastasiia@gmail.com>

--- ai_guide/attractions/admin.py
+++ ai_guide/attractions/admin.py
@@ -1,1 +1,14 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Attraction
+
+
+class AttractiontAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'object_name', 'location',)
+    list_filter = ('object_name',)
+    empty_value_display = '-empty-'
+    search_fields = ('object_name',)
+    sortable_by = ('object_name',)
+
+
+admin.site.register(Attraction, AttractiontAdmin)
