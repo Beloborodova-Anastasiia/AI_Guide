a511fdb21904d5b3da3a95d1588a476dd9ac1771
User <beloborodova.anastasiia@gmail.com>
2024-05-15 11:23:31 +0100

    fixing openia err

--- ai_guide/clients/config.py
+++ ai_guide/clients/config.py
@@ -1,6 +1,6 @@
 MAX_TOKENS = 3800
-MESSAGE = 'Tell me about'
-MODEL = 'gpt-3.5-turbo'
+MESSAGE = "Tell me about"
+MODEL = "gpt-3.5-turbo"
 SYSTEM_MSG = """
 You are a historian. Tell about a historical landmarks in a joyful and
 simple way, provide factual info. The information should start with the object
@@ -11,14 +11,14 @@ Use landmarks name in the language query.
 Return your answer in the following JSON format without formatting:
 '''
 {
-object_name: "",
-location: "",
-content: "",
+"object_name": "",
+"location": "",
+"content": "",
 }
 '''
 """
 TEMPERATURE = 0.8
-REGION_NAME = 'eu-west-2'
-VOICE_ID = 'Brian'
-OUTPUT_FORMAT = 'mp3'
-MEDIA_PATH = 'media/audio/'
+REGION_NAME = "eu-west-2"
+VOICE_ID = "Brian"
+OUTPUT_FORMAT = "mp3"
+MEDIA_PATH = "media/audio/"
--- ai_guide/clients/open_ai_client.py
+++ ai_guide/clients/open_ai_client.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Optional
 
 import openai
 from dotenv import load_dotenv
@@ -18,7 +19,7 @@ class OpenAiClient:
     def __init__(self) -> None:
         openai.api_key = OPEN_AI_API_KEY
 
-    def get_answer(self, query: str) -> AttractionInfo or None:
+    def get_answer(self, query: str) -> Optional[AttractionInfo]:
         message = MESSAGE + f'{query}'
         try:
             response = openai.ChatCompletion.create(
@@ -30,9 +31,10 @@ class OpenAiClient:
                 temperature=TEMPERATURE,
                 max_tokens=MAX_TOKENS
             )
+            print(response["choices"][0]["message"]["content"])
             serializer = AttractionInfoSerializer(
                 data=json.loads(
-                    response["choices"][0]['message']['content'],
+                    response["choices"][0]["message"]["content"],
                     strict=False
                 )
             )
@@ -43,10 +45,12 @@ class OpenAiClient:
                     content=serializer.data['content']
                 )
                 return attraction_info
+            # return None
 
             # in the future report to logger 'Open_AI respons is not correct'
             return None
-        except Exception:
+        except Exception as err:
+            print(err)
             return None
         # in the future report to logger
         # 'No answer from Open_AI or answer is not correct'
